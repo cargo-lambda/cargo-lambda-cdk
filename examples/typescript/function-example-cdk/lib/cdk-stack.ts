73200c518607f1de6be8e8dc8d847bc03384cacc
David Calavera <david.calavera@gmail.com>
2023-09-16 12:19:01 -0700

    Move TS files to their own subdirectories
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- examples/typescript/function-example-cdk/lib/cdk-stack.ts
+++ examples/typescript/function-example-cdk/lib/cdk-stack.ts
@@ -7,12 +7,12 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const func = new RustFunction(this, 'rust-function', {
-      manifestPath: path.join(__dirname, '..', '..')
+    const func = new RustFunction(this, 'Rust Function', {
+      manifestPath: path.join(__dirname, '..', '..', '..', 'rust-function')
     });
 
-    new cdk.CfnOutput(this, 'function name', {
-      value: func.functionName,
+    new cdk.CfnOutput(this, 'function ARN', {
+      value: func.functionArn,
     });
   }
 }
