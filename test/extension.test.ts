3552641b9d5e49db05782aa39eb371d1685661b3
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2025-03-13 19:24:34 +0100

    feat: add `architecture` prop to `RustExtension` (#77)
    * feat: add `architecture` prop to `RustExtension`
    
    * fix readme import
    
    * add test

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,6 +1,8 @@
 import { join } from 'path';
 import { env } from 'process';
 import { App, Stack } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { Architecture } from 'aws-cdk-lib/aws-lambda';
 import { RustExtension, cargoLambdaVersion } from '../src/index';
 
 const forcedDockerBundling = !!env.FORCE_DOCKER_RUN || !cargoLambdaVersion();
@@ -45,6 +47,29 @@ describe('CargoLambda.RustExtension', () => {
     });
 
     test('bundle extension', () => {
+      app.synth();
+    });
+  });
+
+  describe('With ARM64 architecture', () => {
+    const app = new App();
+    const stack = new Stack(app);
+    const testSource = join(__dirname, 'fixtures/single-package');
+
+    new RustExtension(stack, 'rust extension arm64', {
+      manifestPath: testSource,
+      architecture: Architecture.ARM_64,
+    });
+
+    test('bundle extension with ARM64 architecture', () => {
+      // Synthesize the stack to a CloudFormation template
+      const template = Template.fromStack(stack);
+
+      // Verify that the Lambda layer has ARM64 in compatibleArchitectures
+      template.hasResourceProperties('AWS::Lambda::LayerVersion', {
+        CompatibleArchitectures: ['arm64'],
+      });
+
       app.synth();
     });
   });
